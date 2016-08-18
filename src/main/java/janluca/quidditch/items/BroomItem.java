4ca1a1344dcf9804eceb9a4a9ed244bb70c31d2c
LueRader <laurin.kirsch@gmail.com>
2016-08-18 22:24:57 +0200

    Add files via upload
    Right Imports/Folder

--- src/main/java/janluca/quidditch/items/BroomItem.java
+++ src/main/java/janluca/quidditch/items/BroomItem.java
@@ -1,11 +1,25 @@
 package janluca.quidditch.items;
 
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.swing.plaf.ActionMapUIResource;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by Jan-Luca on 16.08.2016.
@@ -17,6 +31,21 @@ public class BroomItem extends Item {
         GameRegistry.register(this);
 
 
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public ActionResult<ItemStack> onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn, EnumHand hand)
+    {
+        if(!worldIn.isRemote) {
+            playerIn.addChatMessage(new TextComponentString("Hallo"));
+
+            EntityZombie z = new EntityZombie(worldIn);
+            z.setPosition(playerIn.posX, playerIn.posY + 30, playerIn.posZ);
+            worldIn.spawnEntityInWorld(z);
+        }
+
+        return new ActionResult(EnumActionResult.PASS, itemStackIn);
     }
 
     @SideOnly(Side.CLIENT)
