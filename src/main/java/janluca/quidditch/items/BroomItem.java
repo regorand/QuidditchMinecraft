ab44701c39f26c12f5f4ed1ef7b75999107670a6
LueRader <laurin.kirsch@gmail.com>
2016-08-21 01:12:06 +0200

    Add files via upload
    Now throws snowball on offHandUse();

--- src/main/java/janluca/quidditch/items/BroomItem.java
+++ src/main/java/janluca/quidditch/items/BroomItem.java
@@ -1,12 +1,10 @@
 package janluca.quidditch.items;
 
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.client.resources.Language;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntitySnowball;
 import net.minecraft.entity.projectile.EntityThrowable;
-import net.minecraft.inventory.InventoryBasic;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
@@ -18,7 +16,8 @@ import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import org.lwjgl.Sys;
+
+import java.util.Arrays;
 
 
 /**
@@ -36,16 +35,15 @@ public class BroomItem extends Item {
     @Override
     public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
 
-        if(entityIn instanceof EntityPlayer){
-            EntityPlayer player = (EntityPlayer) entityIn;
-            if(!worldIn.isRemote && player.inventory.getStackInSlot(40).getUnlocalizedName().equals(this.getUnlocalizedName())){
-                throwSnowball(worldIn, (EntityPlayer) entityIn);
+        if(!worldIn.isRemote) {
+            if (entityIn instanceof EntityPlayer) {
+                EntityPlayer player = (EntityPlayer) entityIn;
+                if(player.inventory.offHandInventory[0] != null && player.inventory.offHandInventory[0].getUnlocalizedName().equals(this.getUnlocalizedName())) {
+                    throwSnowball(worldIn, player);
+                }
             }
         }
-
-
     }
-
     @Override
     @SideOnly(Side.CLIENT)
     public ActionResult<ItemStack> onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn, EnumHand hand)
@@ -61,7 +59,7 @@ public class BroomItem extends Item {
 
         EntityThrowable z = new EntitySnowball(worldIn);
         Vec3d lookDir = playerIn.getLookVec();
-        z.setPosition(playerIn.posX+lookDir.xCoord, playerIn.posY + (double)playerIn.getEyeHeight() - 0.10000000149011612D, playerIn.posZ+lookDir.zCoord);
+        z.setPosition(playerIn.posX, playerIn.posY + (double)playerIn.getEyeHeight() - 0.10000000149011612D, playerIn.posZ);
         z.setThrowableHeading(lookDir.xCoord, lookDir.yCoord, lookDir.zCoord, 1, 0);
         worldIn.spawnEntityInWorld(z);
 
@@ -71,5 +69,4 @@ public class BroomItem extends Item {
     public void initModel(){
         ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName(), "inventory"));
     }
-
 }
