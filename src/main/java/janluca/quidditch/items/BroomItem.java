74f26c45efd963223d155c66b1a119feb7993585
BuildTools <unconfigured@null.spigotmc.org>
2016-08-20 22:04:06 +0200

    now throws snowballs through onUpdate if Broom in secondary hand

--- src/main/java/janluca/quidditch/items/BroomItem.java
+++ src/main/java/janluca/quidditch/items/BroomItem.java
@@ -2,9 +2,11 @@ package janluca.quidditch.items;
 
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.resources.Language;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntitySnowball;
 import net.minecraft.entity.projectile.EntityThrowable;
+import net.minecraft.inventory.InventoryBasic;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
@@ -16,12 +18,14 @@ import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import org.lwjgl.Sys;
 
 
 /**
  * Created by Jan-Luca on 16.08.2016.
  */
 public class BroomItem extends Item {
+
     public BroomItem(){
         setRegistryName("quidditch", "broom");
         setUnlocalizedName("broom");
@@ -30,19 +34,37 @@ public class BroomItem extends Item {
     }
 
     @Override
+    public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
+
+        if(entityIn instanceof EntityPlayer){
+            EntityPlayer player = (EntityPlayer) entityIn;
+            if(!worldIn.isRemote && player.inventory.getStackInSlot(40).getUnlocalizedName().equals(this.getUnlocalizedName())){
+                throwSnowball(worldIn, (EntityPlayer) entityIn);
+            }
+        }
+
+
+    }
+
+    @Override
     @SideOnly(Side.CLIENT)
     public ActionResult<ItemStack> onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn, EnumHand hand)
     {
         if(!worldIn.isRemote) {
-
-            EntityThrowable z = new EntitySnowball(worldIn);
-            Vec3d lookDir = playerIn.getLookVec();
-            z.setPosition(playerIn.posX+lookDir.xCoord, playerIn.posY + (double)playerIn.getEyeHeight() - 0.10000000149011612D, playerIn.posZ+lookDir.zCoord);
-            z.setThrowableHeading(lookDir.xCoord, lookDir.yCoord, lookDir.zCoord, 1, 0);
-            worldIn.spawnEntityInWorld(z);
+            throwSnowball(worldIn, playerIn);
         }
 
         return new ActionResult(EnumActionResult.PASS, itemStackIn);
+    }
+
+    public void throwSnowball(World worldIn, EntityPlayer playerIn){
+
+        EntityThrowable z = new EntitySnowball(worldIn);
+        Vec3d lookDir = playerIn.getLookVec();
+        z.setPosition(playerIn.posX+lookDir.xCoord, playerIn.posY + (double)playerIn.getEyeHeight() - 0.10000000149011612D, playerIn.posZ+lookDir.zCoord);
+        z.setThrowableHeading(lookDir.xCoord, lookDir.yCoord, lookDir.zCoord, 1, 0);
+        worldIn.spawnEntityInWorld(z);
+
     }
 
     @SideOnly(Side.CLIENT)
